0aa9c2008ecd659a35aa41a97a46ea9bfac7d62a
gustavo <gustavodevaler@gmail.com>
2020-10-04 03:30:32 -0300

    Image resize

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -66,23 +66,64 @@ public function store(Request $request)
     $producto->seller_id = Auth::id();
     
     $path_storage = storage_path().'/app/public/images/';
-    
-	if ($request->file('imagen1') != null) {
+    $canvas = Image::canvas(800,800, '#fff');
+
+    if ($request->file('imagen1') != null) {
         $imagen1 = $request->file('imagen1');
         $img = Image::make($imagen1->getPathName());
+        if ($img->width() > $img->height()) {
+            $img->resize(800, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $canvas->insert($img, 'top-left', 0, (800 - $img->height())/2);
+        }
+        if ($img->height() > $img->width() ) {
+            $img->resize(null, 800, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $canvas->insert($img, 'top-left',(800 - $img->width())/2 ,0 );
+        }
+
+        if ($img->width() == $img->height()) {
+             $img->resize(800, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $canvas->insert($img, 'top-left', 0,0);
+        }
+        
+        
         $img_name = time().$imagen1->getClientOriginalName();	
-		$img->save($path_storage.$img_name);
+		$canvas->save($path_storage.$img_name);
         $producto->image1 = '/images/'.$img_name;       
-	}
-	
-	if ($request->file('imagen2') != null) {
+    } 
+// -------------------------imagen2 
+     $canvas2 = Image::canvas(800,800, '#fff');
+    if ($request->file('imagen2') != null) {       
         $imagen2 = $request->file('imagen2');
         $img2 = Image::make($imagen2->getPathName());
+        if ($img2->width() > $img2->height()) {
+            $img2->resize(800, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $canvas2->insert($img2, 'top-left', 0, (800 - $img2->height())/2);
+        }
+        if ($img2->height() > $img2->width() ) {
+            $img2->resize(null, 800, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $canvas2->insert($img2, 'top-left',(800 - $img2->width())/2 ,0 );
+        }
+        if ($img2->width() == $img2->height()) {
+            $img2->resize(800, null, function ($constraint) {
+               $constraint->aspectRatio();
+           });
+           $canvas2->insert($img2, 'top-left', 0,0);
+       }       
+        
         $img_name2 = time().$imagen2->getClientOriginalName();	
-		$img2->save($path_storage.$img_name2);
+		$canvas2->save($path_storage.$img_name2);
         $producto->image2 = '/images/'.$img_name2;       
-	}
-	
+    } 	
 	
 	$producto->save();
 	
