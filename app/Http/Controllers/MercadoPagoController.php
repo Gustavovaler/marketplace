967f9eae8ddc86c1852749562c81efefeaaf3aae
gustavo <gustavodevaler@gmail.com>
2021-01-08 00:55:59 -0300

    creating mercadopago controller's methods

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class MercadoPagoController extends Controller
 {
-    //
+    public function seller_auth_form(){
+        return "seller_auth_form";
+    }
+
+    public function get_link_up_code(){
+        return "este retorna el code de autorizacion";
+    }
+
+    public function get_seller_credentials(){
+        return "este retorna las credenciales";
+    }
+
+    
 }
